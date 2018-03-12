d5a7d73a1837994a2071fd466a4e27eae519f13c
SilviuMa <smrevan@yahoo.co.uk>
2018-03-13 01:46:19 +0200

    Update methodFactory.js

--- front-end dev/methodFactory.js
+++ front-end dev/methodFactory.js
@@ -1,32 +1,65 @@
 import axios from 'axios';
 
 const methodRequests = {
-    GET: function(url, updateState) {
+    GET: function(url, updateState, toggleLoadingState) {
+        toggleLoadingState();
         axios.get(url)
         .then(function (response) {
             updateState(JSON.stringify(JSON.parse(response.data.body),null,2));
+            toggleLoadingState();
         })
         .catch(function (error) {
             updateState(error);
+            toggleLoadingState();
         });
     },
-    POST: function(url, updateState, body) {
+    POST: function(url, updateState, toggleLoadingState, body) {
+        toggleLoadingState();
         axios.post(url, body)
           .then(function (response) {
             updateState(JSON.stringify(JSON.parse(response.data.body),null,2));
+            toggleLoadingState();
           })
           .catch(function (error) {
             updateState(error);
+            toggleLoadingState();
           });
     },
-    PUT: function(url, updateState, body) {
+    POSTarrayBody: function(url, updateState, toggleLoadingState, body) {
+        toggleLoadingState();
+        axios.post(url, body)
+          .then(function (response) {
+            updateState(response.data);
+            toggleLoadingState();
+          })
+          .catch(function (error) {
+            updateState(error);
+            toggleLoadingState();
+          });
+    },
+    PUT: function(url, updateState, toggleLoadingState, body) {
+        toggleLoadingState();
         axios.put(url, body)
         .then(function (response) {
             updateState(JSON.stringify(JSON.parse(response.data.body),null,2));
+            toggleLoadingState();
         })
         .catch(function (error) {
             updateState(error);
+            toggleLoadingState();
         });
+    },
+    PUTarrayBody: function(url, updateState, toggleLoadingState, body) {
+        toggleLoadingState();
+        axios.put(url, body)
+          .then(function (response) {
+            updateState(response.data);
+            toggleLoadingState();
+          })
+          .catch(function (error) {
+            updateState(error);
+            toggleLoadingState();
+          });
     }
 }
 
